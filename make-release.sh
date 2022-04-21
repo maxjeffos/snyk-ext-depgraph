f771c5376d3954e8145199ce2c4b27d4783b54bc
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2022-04-21 09:59:03 -0400

    flesh out meta and args

--- make-release.sh
+++ make-release.sh
@@ -1,3 +1,3 @@
-go build -o release/snyk-ext-depgraph cmd/snyk-ext-depgraph/main.go
+go build -o release/snyk_ext_depgraph_darwin_amd64 cmd/snyk-ext-depgraph/main.go
 cp args.json release/args.json
 cp meta.json release/meta.json
